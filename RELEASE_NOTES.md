e971d7423241fedb941d1f1b171eed66260c8a1c
Chris Lanzo <cjlanzo92@gmail.com>
2022-04-16 00:00:13 -0400

    removed RaidNotesTBC specific info

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,5 +1,1 @@
-## 1.0.1 (2022-03-14)
-* Removed org test code
-
-## 1.0.0 (2022-03-14)
-* Initial release of addon
+## 1.0.0 (2022-04-15)
--- build.cmd
+++ build.cmd
@@ -2,27 +2,28 @@
 cls
 
 set target=%1
-set addon_folder="C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns\RaidNotesTBC"
+set addon_name="addon-name"
+set addon_folder="C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns"
+set publish_folder="%addon_folder%\%addon_name%" 
 
 if exist build rmdir /Q /S build
 mkdir build
 mkdir build\Libs
 
 xcopy src build
-xcopy src\GUI build /E
 xcopy src\Libs build\Libs /E
 copy RELEASE_NOTES.md build
 copy README.md build
 
 if %target% == publish (
-    if exist %addon_folder% rmdir /Q /S %addon_folder%
-    mkdir %addon_folder%
-    xcopy build %addon_folder% /E
+    if exist %publish_folder% rmdir /Q /S %publish_folder%
+    mkdir %publish_folder%
+    xcopy build %publish_folder% /E
 )
 
 if %target% == release (
     if exist release rmdir /Q /S release
     mkdir release
-    mkdir release\RaidNotesTBC
-    xcopy build release\RaidNotesTBC /E
+    mkdir release\%addon_name%
+    xcopy build release\%addon_name% /E
 )
